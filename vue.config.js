edf075b6846e44d96b8a98af3fec371038346078
Nicolas Walcker de Miranda <walcker_@outlook.com>
2022-04-20 09:42:11 -0300

    Initial commit

---
+++ vue.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  configureWebpack: {
+    plugins: [
+      //
+    ],
+    optimization: {
+      splitChunks: {
+        chunks: 'all',
+        minSize: 9000,
+        maxSize: 140000,
+      },
+    },
+  },
+  css: {
+    loaderOptions: {
+      scss: {
+        additionalData: `
+        @import "@/assets/sass/base/_colors.scss";
+        @import "@/assets/sass/base/_variables.scss";
+        @import "@/assets/sass/base/_breakpoints.scss";
+        @import "@/assets/sass/base/_mixins.scss";
+        @import "@/assets/sass/base/_utils.scss";
+        @import "@/assets/sass/base/_keyframes.scss";
+        `,
+      },
+    },
+  },
+};
